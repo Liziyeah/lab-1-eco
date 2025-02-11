742753e27a69d0b604630f0e80257394d5c98f14
Mariana <marianitalizcanor@gmail.com>
2025-02-11 11:15:40 -0500

    tercera api

--- index.js
+++ index.js
@@ -35,3 +35,43 @@ const callSecondData = document.getElementById('second-api');
 callSecondData.addEventListener('click', getSecondData);
 
 //Tercer llamado
+
+const categories = {
+    limit: [],
+    name: [],
+    type: [],
+}
+
+const getThirdData = async () => {
+    try{
+        const response = await fetch('https://api.jikan.moe/v4/anime');
+        const data = await response.json();
+        categories.limit = [];
+        categories.name = [];
+        categories.type = [];
+        
+        data.data.forEach(category => {
+            categories.limit.push(category.mal_id);
+            categories.name.push(category.title_english);
+            categories.type.push(category.type);
+            
+        });       
+        showCategories();
+    }catch(error) {
+        console.error(error);
+        
+    }
+}
+
+const showCategories = () => {
+    const limitSection = document.getElementById('limit-section');
+    const nameSection = document.getElementById('name-section');
+    const typeSection = document.getElementById('type-section');
+
+    limitSection.innerHTML += categories.limit.map(id => `<p>${id}</p>`).join('');
+    nameSection.innerHTML += categories.name.map(name => `<p>${name || "No name"}</p>`).join('');
+    typeSection.innerHTML += categories.type.map(type => `<p>${type || "No type"}</p>`).join('');
+}
+
+const callThirdData = document.getElementById('button-third');
+callThirdData.addEventListener('click', getThirdData);
